be160ec20519c923041b17f9e071f9328f9efa35
valbona1992 <valbona12@gmail.com>
2021-07-12 14:35:16 -0500

    Add information to READme file

--- Develop/script.js
+++ Develop/script.js
@@ -1,58 +1,38 @@
-/* User Story
-AS AN employee with a busy schedule
-I WANT to add important events to a daily planner
-SO THAT I can manage my time effectively
-Acceptance Criteria
-GIVEN I am using a daily planner to create a schedule
-WHEN I open the planner
-THEN the current day is displayed at the top of the calendar
-WHEN I scroll down
-THEN I am presented with timeblocks for standard business hours
-WHEN I view the timeblocks for that day
-THEN each timeblock is color coded to indicate whether it is in the past, present, or future
-WHEN I click into a timeblock
-THEN I can enter an event
-WHEN I click the save button for that timeblock
-THEN the text for that event is saved in local storage
-WHEN I refresh the page
-THEN the saved events persist */
-
-
-
-
+// adds the current day to be displayed on the calendar
 
 var date = moment().format('dddd, MMMM Do YYYY');
 $("#currentDay").text(date);
 
-
+// loops through each hour of the work day and determines if its past, present, or future
 let textblocks = $(".text-block");
 let timeBlocks = $(".time-block");
-for (let i = 0; i< timeBlocks.length; i++) {
+for (let i = 0; i < timeBlocks.length; i++) {
     let currentHour = parseInt(moment().format("H"));
-    let blockHour =  parseInt($(timeBlocks[i]).attr("data-number"));
+    let blockHour = parseInt($(timeBlocks[i]).attr("data-number"));
 
-    if (blockHour < currentHour){
+    if (blockHour < currentHour) {
         $(textblocks[i]).addClass("past");
     } else if (blockHour === currentHour) {
         $(textblocks[i]).addClass("present");
     } else {
         $(textblocks[i]).addClass("future");
     }
-
+    // store the text to local storage
     let key = moment().format("MMMM Do YYYY ") + blockHour;
     let possibleStorage = localStorage.getItem(key);
-    
+
     $(textblocks[i]).text(possibleStorage);
 
 }
 
+// saves the text for the selected textarea
 
-
-function saveButton(){
+function saveButton() {
     let buttonHourNumber = $(this).parent().attr("data-number");
     let textAreaForHour = $(this).siblings(".text-block").val();
     let key = moment().format("MMMM Do YYYY ") + buttonHourNumber;
     localStorage.setItem(key, textAreaForHour);
 }
 
+// click event for saving the text 
 $("button").on("click", saveButton);
--- README.md
+++ README.md
@@ -1,1 +1,19 @@
 # homework05-workdayscheduler
+
+This is the link to the following assignment: [link](https://valbona1992.github.io/homework05-workdayscheduler/)
+
+## Summary
+This assignment required me to modify code given a starter code for a work day scheduler web app. 
+
+Below is a summary of what I implemented. 
+
+* Added HTML to include basic information for each work day hour of the day. 
+* Added classes to the HTML code to reference the given CSS file.  
+* Added code to implement Moment.js for the current date to be displayed on the browser. 
+* Added code to loop through the timeblocks to determine past, present, or future hours. 
+* Added a function to save the added text into local storage
+
+
+## Screenshot
+![screenshot](assets/WorkDayScheduler.png) 
+
